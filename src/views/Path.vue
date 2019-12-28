bc378537ed6b30bc13ea65c3485d292fdea73a62
Joaquim Perez <jivkim@gmail.com>
2019-12-28 22:04:27 +0100

    stop motion in component Path

--- src/views/Path.vue
+++ src/views/Path.vue
@@ -6,14 +6,31 @@
 export default {
   data() {
     return {
+      folder: "",
       src: "",
-      images: []
+      images: [],
+      frame: 0
     };
   },
   created() {
-    var folder = this.$route.name;
-    this.images = this.$root.paths[folder].images;
-    this.src = "images/" + folder + "/" + this.images[0];
+    this.folder = this.$route.name;
+    this.images = this.$root.paths[this.folder].images;
+    this.src = "images/" + this.folder + "/" + this.images[0];
+  },
+  mounted() {
+    this.nextFrame();
+  },
+  methods: {
+    nextFrame() {
+      var image = this.images[this.frame];
+      if (image) {
+        this.src = "images/" + this.folder + "/" + image;
+        this.frame++;
+        setTimeout(this.nextFrame, 100);
+      } else {
+        console.log("fin");
+      }
+    }
   }
 };
 </script>
