6d730ba8ae57c748178b0483a87d7d8692bf0021
Joaquim Perez <jivkim@gmail.com>
2020-01-03 19:13:11 +0100

    no fixed background height

--- src/views/Panorama.vue
+++ src/views/Panorama.vue
@@ -3,7 +3,7 @@
     :style="{
       backgroundImage: 'url(' + $route.meta.image.src + ')',
       height: '500px',
-      backgroundSize: 'auto 500px',
+      backgroundSize: 'auto 100%',
       backgroundPosition: 'calc(50% - ' + x + 'px) center'
     }"
     @click="x += 100"
