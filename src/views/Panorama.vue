abc1aec89ff2605e54e73f6c0f1156d27d5a80e7
Joaquim Perez <jivkim@gmail.com>
2020-01-03 15:41:45 +0100

    try vuejs-panorama

--- src/views/Panorama.vue
+++ src/views/Panorama.vue
@@ -1,3 +1,20 @@
 <template>
-  <photo :src="'images/panoramas/' + $route.name + '.jpg'" />
+  <vuejs-panorama
+    :source="'images/panoramas/' + $route.name + '.jpg'"
+    @viewchange="move"
+    :animationTime="0"
+  />
 </template>
+
+<script>
+import VuejsPanorama from "vuejs-panorama";
+
+export default {
+  components: { VuejsPanorama },
+  methods: {
+    move(view) {
+      // todo: move marks
+    }
+  }
+};
+</script>
