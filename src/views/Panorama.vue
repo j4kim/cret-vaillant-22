68fb071d213626c12934d97b897ca89babc1ddd1
Joaquim Perez <jivkim@gmail.com>
2020-01-03 15:58:58 +0100

    homemade panorama inspired by vue-panorama mechanism

--- src/views/Panorama.vue
+++ src/views/Panorama.vue
@@ -1,3 +1,24 @@
 <template>
-  <photo :src="'images/panoramas/' + $route.name + '.jpg'" />
+  <div
+    :style="{
+      backgroundImage: 'url(images/panoramas/' + $route.name + '.jpg)',
+      height: '500px',
+      backgroundSize: 'auto 500px',
+      backgroundPosition: -this.x + 'px'
+    }"
+    @click="move"
+  />
 </template>
+
+<script>
+export default {
+  data() {
+    return { x: 0 };
+  },
+  methods: {
+    move() {
+      this.x += 100;
+    }
+  }
+};
+</script>
